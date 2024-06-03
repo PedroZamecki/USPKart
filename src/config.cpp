ea52702d8787eecfb37d8e6c73de95912d3573f0
pedro.zamecki <pedro.zamecki@acrelec.com>
2024-06-03 07:17:00 -0300

    - First implementation with CMake and better changes from the first version (WIN and LINUX compatible)

---
+++ src/config.cpp
@@ -0,0 +1,64 @@
+#include "config.hpp"
+
+const char* configPath = "../config/config.json";
+
+void Configuration::readConfigurationFile() {
+	// Create the directory if it doesn't exist.
+	std::string directory = "../config";
+	if (!std::filesystem::exists(directory)) {
+		std::filesystem::create_directory(directory);
+	}
+
+	// Read the JSON file with the configuration.
+	std::fstream file;
+	file.open(configPath, std::ios::in);
+
+	if (!file.is_open()) {
+		std::cerr << "Failed to open the configuration file" << std::endl;
+		std::cout << "Creating a new configuration file..." << std::endl;
+		// Create a new configuration file.
+		std::fstream newFile;
+		newFile.open(configPath, std::ios::out);
+		if (!newFile.is_open()) {
+			std::cerr << "Failed to create the configuration file" << std::endl;
+			return;
+		}
+		newFile.close();
+		writeConfigurationFile();
+		return;
+	}
+
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	file.close();
+
+	// Parse the JSON file.
+	nlohmann::json json = nlohmann::json::parse(buffer.str());
+	
+	// Set the configuration.
+	width = json["width"];
+	height = json["height"];
+	resizable = json["resizable"];
+	fullScreen = json["fullScreen"];
+	borderless = json["borderless"];
+}
+
+void Configuration::writeConfigurationFile() {
+	// Write the JSON file with the configuration.
+	nlohmann::json json;
+	json["width"] = width;
+	json["height"] = height;
+	json["resizable"] = resizable;
+	json["fullScreen"] = fullScreen;
+	json["borderless"] = borderless;
+
+	std::fstream file;
+	file.open(configPath, std::ios::out);
+	if (!file.is_open()) {
+		std::cerr << "Failed to open the configuration file" << std::endl;
+		return;
+	}
+
+	file << json.dump(4);
+	file.close();
+}
---
+++ src/config.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+class Configuration {
+	private:
+		unsigned int width;
+		unsigned int height;
+		bool resizable;
+		bool fullScreen;
+		bool borderless;
+
+		// Destructor
+		~Configuration() {
+			writeConfigurationFile();
+		}
+	
+	public:
+		Configuration(	unsigned int width = 800, 
+						unsigned int height = 600,
+						bool resizable = false,
+						bool fullScreen = false,
+						bool borderless = false) : width(width), height(height), resizable(resizable), fullScreen(fullScreen), borderless(borderless) {
+			readConfigurationFile();
+		}
+		unsigned int getWidth() const { return width; }
+		unsigned int getHeight() const { return height; }
+		bool isResizable() const { return resizable; }
+		bool isFullScreen() const { return fullScreen; }
+		bool isBorderless() const { return borderless; }
+		void setWidth(unsigned int width) { this->width = width; }
+		void setHeight(unsigned int height) { this->height = height; }
+		void setResizable(bool resizable) { this->resizable = resizable; }
+		void setFullScreen(bool fullScreen) { this->fullScreen = fullScreen; }
+		void setBorderless(bool borderless) { this->borderless = borderless; }
+
+		void readConfigurationFile();
+		void writeConfigurationFile();
+};
