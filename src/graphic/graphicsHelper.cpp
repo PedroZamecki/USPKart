50ce1ede679a4d2588caa842b7b6299717080c79
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2024-10-03 23:29:50 -0300

    Added singleton classes, removed legacy drawing methods.

--- src/graphic/graphicsHelper.cpp
+++ src/graphic/graphicsHelper.cpp
@@ -1,90 +1,14 @@
-#include "graphicsHelper.hpp"
-
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+
+#include "graphicsHelper.hpp"
 #include <fstream>
 
-#include <graphic/drawingHelper.hpp>
 #include <iostream>
-#include <sstream>
 #include <vector>
 
 #include <AL/alc.h>
 
-std::string readShader(const std::string &filename)
-{
-	const std::ifstream file(filename);
-	if (!file.is_open())
-	{
-		std::cerr << "Error reading shader: " << filename << std::endl;
-		throw std::runtime_error("Failed to open shader file.");
-	}
-
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	return buffer.str();
-}
-
-GLuint compileShader(GLenum const type, const std::string &source)
-{
-	const GLuint shader = glCreateShader(type);
-	const char *src = source.c_str();
-	glShaderSource(shader, 1, &src, nullptr);
-	glCompileShader(shader);
-
-	int success;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		char infoLog[512];
-		glGetShaderInfoLog(shader, 512, nullptr, infoLog);
-		std::cerr << "ERROR::SHADER::COMPILATION_FAILED" << std::endl << infoLog << std::endl;
-		glDeleteShader(shader); // Guarantee that the shader is deleted in case of failure
-		throw std::runtime_error("Shader compilation failed");
-	}
-
-	return shader;
-}
-
-GLuint createShaderProgram(const std::vector<GLuint> &shaders)
-{
-	const GLuint program = glCreateProgram();
-	for (const auto shader : shaders)
-	{
-		glAttachShader(program, shader);
-	}
-	glLinkProgram(program);
-
-	int success;
-	glGetProgramiv(program, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		char infoLog[512];
-		glGetProgramInfoLog(program, 512, nullptr, infoLog);
-		std::cerr << "ERROR::PROGRAM::LINKING_FAILED" << std::endl << infoLog << std::endl;
-		glDeleteProgram(program); // Guarantee that the shader is deleted in case of failure
-		throw std::runtime_error("Program linking failed");
-	}
-
-	return program;
-}
-
-GLuint GraphicsHelper::loadShaders()
-{
-	const std::string vertexShaderSource = readShader("shaders/shader.vs");
-	const std::string fragmentShaderSource = readShader("shaders/shader.fs");
-
-	GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
-	GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
-
-	const GLuint shaderProgram = createShaderProgram({vertexShader, fragmentShader});
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	glUseProgram(shaderProgram);
-	return shaderProgram;
-}
+#include "game/config.hpp"
 
 void GraphicsHelper::configureEnvironment()
 {
@@ -121,7 +45,6 @@ void GraphicsHelper::configureEnvironment()
 GraphicsHelper::~GraphicsHelper()
 {
 	// Cleanup
-	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 
 	// Close OpenAL
@@ -132,9 +55,10 @@ GraphicsHelper::~GraphicsHelper()
 	alcCloseDevice(device);
 }
 
-void *GraphicsHelper::createWindow(const char *title, const Configuration *c, const void *icon)
+void *GraphicsHelper::createWindow(const char *title, const GLFWimage *icon)
 {
 	GLFWwindow *window;
+	const auto c = Configuration::getInstance();
 
 	if (!glfwInit())
 	{
@@ -168,7 +92,7 @@ void *GraphicsHelper::createWindow(const char *title, const Configuration *c, co
 		throw std::runtime_error("Failed to create GLFW window");
 	}
 
-	glfwSetWindowIcon(window, 1, (GLFWimage*)icon);
+	glfwSetWindowIcon(window, 1, (GLFWimage *)icon);
 
 	glfwSetWindowPos(window, c->posX, c->posY);
 	glfwMakeContextCurrent(window);
@@ -230,60 +154,4 @@ void GraphicsHelper::setupOpenGL(const int width, const int height)
 
 	/* Setup our viewport. */
 	glViewport(0, 0, width, height);
-}
-
-void GraphicsHelper::generateShaders(GLuint &programID, const char *vertexShader, const char *fragmentShader)
-{
-	// Create the shaders
-	const GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	const GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Compile the vertex shader
-	glShaderSource(vertexShaderID, 1, &vertexShader, nullptr);
-	glCompileShader(vertexShaderID);
-
-	// Check the vertex shader
-	GLint result = GL_FALSE;
-	int infoLogLength;
-	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(vertexShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if (infoLogLength > 0)
-	{
-		std::vector<char> vertexShaderErrorMessage(infoLogLength + 1);
-		glGetShaderInfoLog(vertexShaderID, infoLogLength, nullptr, vertexShaderErrorMessage.data());
-		std::cerr << vertexShaderErrorMessage.data() << std::endl;
-	}
-
-	// Compile the fragment shader
-	glShaderSource(fragmentShaderID, 1, &fragmentShader, nullptr);
-	glCompileShader(fragmentShaderID);
-
-	// Check the fragment shader
-	glGetShaderiv(fragmentShaderID, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(fragmentShaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if (infoLogLength > 0)
-	{
-		std::vector<char> fragmentShaderErrorMessage(infoLogLength + 1);
-		glGetShaderInfoLog(fragmentShaderID, infoLogLength, nullptr, fragmentShaderErrorMessage.data());
-		std::cerr << fragmentShaderErrorMessage.data() << std::endl;
-	}
-
-	// Link the program
-	programID = glCreateProgram();
-	glAttachShader(programID, vertexShaderID);
-	glAttachShader(programID, fragmentShaderID);
-	glLinkProgram(programID);
-
-	// Check the program
-	glGetProgramiv(programID, GL_LINK_STATUS, &result);
-	glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
-	if (infoLogLength > 0)
-	{
-		std::vector<char> programErrorMessage(infoLogLength + 1);
-		glGetProgramInfoLog(programID, infoLogLength, nullptr, programErrorMessage.data());
-		std::cerr << programErrorMessage.data() << std::endl;
-	}
-
-	glDeleteShader(vertexShaderID);
-	glDeleteShader(fragmentShaderID);
 }
