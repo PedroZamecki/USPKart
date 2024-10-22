c255a9c406dbf038b155ffdd6e94b3d62f4b64ea
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2024-10-22 13:30:28 -0300

    Update 0.0.6: - Added more options for movement in the kart

--- src/object/kart.hpp
+++ src/object/kart.hpp
@@ -5,7 +5,8 @@
 
 class Wheel final : public Object
 {
-	bool inverted;
+	bool inverted{false};
+	float steeringAngle{0.0f};
 
 public:
 	explicit Wheel(const Position &pos, const bool inverted = false) :
@@ -13,13 +14,16 @@ public:
 	{
 	}
 
+	void steer(const float value) { steeringAngle += value; }
+
 	glm::mat4 getModel(const glm::mat4 &baseModel) override
 	{
 		auto m_model = baseModel;
 		m_model = glm::translate(m_model, pos.toVec3());
 		m_model = glm::scale(m_model, glm::vec3(scale, scale, scale));
+		m_model = glm::rotate(m_model, steeringAngle, glm::vec3(0.0f, 1.0f, 0.0f));
 		m_model = glm::rotate(m_model, pitch, glm::vec3(1.0f, 0.0f, 0.0f));
-		m_model = glm::rotate(m_model, inverted ?  glm::radians(180.0f) : 0 +  angle , glm::vec3(0.0f, 1.0f, 0.0f));
+		m_model = glm::rotate(m_model, inverted ? glm::radians(180.0f) : 0 + angle, glm::vec3(0.0f, 1.0f, 0.0f));
 		m_model = glm::rotate(m_model, yaw, glm::vec3(0.0f, 0.0f, 1.0f));
 		return m_model;
 	}
@@ -34,6 +38,7 @@ public:
 
 class Kart final : public Object
 {
+	float speed{0.0f};
 	Wheel wheels[4] = {Wheel(pos + Position{10, 4, 12.5} / 16), Wheel(pos + Position{10, 4, -12.5} / 16),
 					   Wheel(pos + Position{-10, 4, 12.5} / 16, true), Wheel(pos + Position{-10, 4, -12.5} / 16, true)};
 
@@ -45,7 +50,7 @@ public:
 		Object::draw(shader, deltaTime, baseModel);
 		for (auto &wheel : wheels)
 		{
-			wheel.run(10 * deltaTime);
+			wheel.adjustPitch(speed * deltaTime * 0.1f);
 			wheel.draw(shader, deltaTime, getModel(baseModel));
 		}
 	}
