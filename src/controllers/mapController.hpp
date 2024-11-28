7b4e311df086419b038ef0a8f44b83f595f5d643
Pedro Zamecki <51971866+PedroZamecki@users.noreply.github.com>
2024-11-27 22:18:30 -0300

    Update 0.3.2: - Fixes in the logic system of the path-finding - TODO: Logic for progression on the track

--- src/controllers/mapController.hpp
+++ src/controllers/mapController.hpp
@@ -5,22 +5,24 @@
 #include <vector>
 
 class Object;
+typedef std::pair<std::pair<int, int>, std::pair<int, int>> Checkpoint ;
 
 class MapController
 {
 	void loadPGM(const std::string &pgmFile);
 	std::vector<std::vector<int>> map;
-	std::vector<std::string> trackObjects;
-	std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> checkpoints;
+	std::vector<Checkpoint> checkpoints;
 
 public:
 	MapController(const std::string &ppmFile,
-				  const std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> &checkpoints);
-	void loadTrackObjects(const std::vector<std::string> &objects);
-	std::vector<std::pair<int, int>> findPath(int x, int y, int checkpointIdx, const std::vector<Object *> &objects);
-	std::vector<std::vector<int>> getWeightedMap(const std::vector<Object *> &objects);
-	void saveModifiedPPM(const std::string &outputFilePath, const std::vector<Object *> &objects, int x, int y,
-						 int checkpointIdx);
+				  const std::vector<Checkpoint> &checkpoints);
+	std::vector<std::pair<int, int>> findPath(int x, int y, std::vector<std::vector<int>> map, int checkpointIdx);
+	void fillPolygon(std::vector<std::vector<int>> *weightedMap, Object *object, int code);
+	void fillLine(std::vector<std::vector<int>> *weightedMap, Checkpoint checkpoint, int code);
+	std::vector<std::vector<int>> getWeightedMap(std::vector<Object *> trackObjects, Object *observer, int checkpointIdx);
+	void saveModifiedPPM(const std::string &outputFilePath, int x, int y,
+						 int checkpointIdx, std::vector<Object *> trackObjects, Object *observer);
+	std::vector<Checkpoint> getCheckpoints() { return checkpoints; }
 };
 
 #endif // MAP_CONTROLLER_HPP
